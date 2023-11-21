86daf2e3d726af934dec05549bf944c9002a2355
Luc Rosenzweig <lucrosenzweig@Lucs-MBP-3.lan>
2023-11-20 19:28:59 -0500

    Started adding training code

---
+++ train/data.py
@@ -0,0 +1,7 @@
+# Dimensions of the input to the model (1000 IO pairs)
+INPUT_DIM = 1000 * 6
+
+# Baseline model dimensions
+BRANCH1_OUTPUTS = 30 * 5
+BRANCH2_OUTPUTS = 30 * 6
+BRANCH3_OUTPUTS = 30 * 26
--- train/main.py
+++ train/main.py
@@ -1,1 +1,5 @@
+import model
 
+if __name__ == "__main__":
+  model.someFunction()
+  print("Hello world")
