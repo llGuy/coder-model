2456357489f9cf1cf13a953f9e92a347d9c4652a
Roshan Prabhakar <roshan@aprabhakar.com>
2023-11-20 16:27:33 -0800

    added serializeInstr

--- data/main.cc
+++ data/main.cc
@@ -7,10 +7,16 @@
 #include <iostream>
 #include <filesystem>
 
+#include <assert.h>
+
 /* Maximum number of characters a program can have. */
 inline constexpr uint32_t kMaxProgramSize = 2048;
 inline constexpr uint32_t kNumRegisters = 3;
 inline constexpr uint32_t kMaxInputs = 3;
+
+// 30 instructions, each with 5-float, 6-float, and 26-float components.
+#define PROGRAM_INSTR_LEN_BYTES (sizeof(float) * (5 + 6 + 26))
+#define PROGRAM_SIZE_BYTES (30 * PROGRAM_INSTR_LEN_BYTES)
 
 enum class Operation {
   Add,
@@ -18,26 +24,53 @@ enum class Operation {
   Mul,
   Div,
   Mov,
+	Nop, 
 
   /* Takes in register and number of lines to skip. */
   BranchLess,
   BranchGreater,
   BranchEqual,
   None
+
 };
 
-char *kOperationNames[] = {
+enum class Operand {
+  Input,
+  Literal,
+  Register,
+  None
+};
+
+struct OperationData {
+  Operation op;
+  OperandData left;
+  OperandData right;
+};
+
+struct OperandData {
+  Operand op;
+
+  /* if op == Input: value takes on [0, numInputs]
+   * if op == Register: value takes on [0, numRegisters]
+   * if op == Literal: value takes on any value */
+  int value;
+};
+
+
+char const *kOperationNames[] = {
   "add",
   "sub",
   "mul",
   "div",
   "mov",
+	/*
   "bl",
   "bg",
   "be"
+	*/
 };
 
-char *serializeString(char *str, char *mem)
+char *serializeString(char const *str, char *mem)
 {
   strcpy(mem, str);
   return mem + strlen(mem);
@@ -66,27 +99,11 @@ inline constexpr uint32_t kNumOperations =
 inline constexpr uint32_t kMinLinesBranch = 2;
 inline constexpr uint32_t kMaxLinesBranch = 5;
 
-enum class Operand {
-  Input,
-  Literal,
-  Register,
-  None
-};
-
 inline constexpr uint32_t kNumOperands =
   (uint32_t)Operand::None;
 
 inline constexpr uint32_t kLiteralRange = 20;
 inline constexpr uint32_t kLiteralOffset = 10;
-
-struct OperandData {
-  Operand op;
-
-  /* if op == Input: value takes on [0, numInputs]
-   * if op == Register: value takes on [0, numRegisters]
-   * if op == Literal: value takes on any value */
-  int value;
-};
 
 char *serializeOperandData(const OperandData &operand, char *mem)
 {
@@ -105,7 +122,7 @@ char *serializeOperandData(const OperandData &operand, char *mem)
 
     case Operand::None: case Operand::Literal: {
       char str[16];
-      sprintf(str, "%d", operand.value);
+      snprintf(str, 16, "%d", operand.value);
       return serializeString(str, mem);
     } break;
   }
@@ -113,11 +130,6 @@ char *serializeOperandData(const OperandData &operand, char *mem)
   return nullptr;
 }
 
-struct OperationData {
-  Operation op;
-  OperandData left;
-  OperandData right;
-};
 
 struct Rand {
   std::random_device dev;
@@ -206,12 +218,97 @@ OperandData generateRValue(ProgramState &state,
 
 Operation generateOperation(Rand &rnd)
 {
+	/*
   if (rnd.next() % 4 == 0) {
     return (Operation)((int)Operation::BranchLess + rnd.next() % 3);
   }
   else {
     return (Operation)(rnd.next() % 5);
   }
+	*/
+	return (Operation) (rnd.next() % 5);
+}
+
+#define INSTR_PROB_VECTOR_LEN ((int) Operation::Nop)
+#define LVAL_PROB_VECTOR_LEN (kNumInputs + kNumRegisters)
+#define RVAL_PROB_VECOTR_LEN (kNuminputs + kNumRegisters + kLiteralRange)
+
+float *serializeInstr(OperationData opData, float *progMem)
+{
+	// If operation is nop/none, assume progMem points to 0'd mem, advance.
+	if (opData.op == Operation::None)
+	{
+		return (float *) ((char *) progMem + PROGRAM_INSTR_LEN_BYTES);
+	}
+	
+	// Test for little-end, fuss if not.
+	uint64_t instr_buf = 255;
+	assert(*(uint8_t *) &instr_buf == 255);	
+
+	// Else, construct the appropriate float vector, and memcpy.
+	float instr_probs[INSTR_PROB_VECTOR_LEN] = {0};
+	float lval_probs[LVAL_PROB_VECTOR_LEN] = {0}; // ordered {inputs..., registers...}
+	float rval_probs[RVAL_PROB_VECOTR_LEN] = {0}; // ordered {inputs..., registers..., literals...}
+	
+	instr_probs[opData.op] = 1.0f;
+
+	switch (opData.left.op)
+	{
+		case Operand::Input: 
+		{
+			lval_probs[opData.left.value] = 1.0f;
+		} break;
+		case Operand::Register:
+		{
+			lval_probs[kNumInputs + opData.left.value] = 1.0f;
+		} break;
+		default:
+		{
+			// Malformed input, fuss.
+			printf("Found an Lvalue not equal to register or input, aborting\n");
+			assert(0);
+		} break;
+	}
+
+	switch (opData.right.op)
+	{
+		case Operand::Input:
+		{
+			rval_probs[opData.right.value] = 1.0f;
+		} break;
+		case Operand::Register:
+		{
+			rval_probs[kNumInputs + opData.right.value] = 1.0f;
+		} break;
+		case Operand::Literal:
+		{
+			rval_probs[kNumInputs + kNumRegisters + kLiteralOffset + opData.right.value] = 1.0f;
+		} break;
+		default: {} break;
+	}
+	
+	printf("Generated arrays:\n");
+	printf("instr probs: ");
+	for (int i = 0; i < INSTR_PROB_VECTOR_LEN; ++i)
+	{
+		std::cout << std::fixed << std::setprecision(2) << instr_probs[i] << " ";
+	}
+	std::cout << std::endl;
+	printf("instr probs: ");
+	for (int i = 0; i < INSTR_PROB_VECTOR_LEN; ++i)
+	{
+		std::cout << std::fixed << std::setprecision(2) << instr_probs[i] << " ";
+	}
+	std::cout << std::endl;
+	printf("lvalue probs: ");
+	for (int i = 0; i < INSTR_PROB_VECTOR_LEN; ++i)
+	{
+		std::cout << std::fixed << std::setprecision(2) << instr_probs[i] << " ";
+	}
+	std::cout << std::endl;
+
+	
+	return NULL;
 }
 
 char *pushInstructions(ProgramState &state,
@@ -229,7 +326,9 @@ char *pushInstructions(ProgramState &state,
       opData.left = generateLValue(state, rnd, instruction);
       opData.right = generateRValue(state, rnd);
     } break;
+		default: {} break;
 
+		/*
     case Operation::BranchLess:
     case Operation::BranchGreater:
     case Operation::BranchEqual: {
@@ -237,6 +336,7 @@ char *pushInstructions(ProgramState &state,
       opData.right = { Operand::None, rnd.next(kMinLinesBranch, 
                                                kMaxLinesBranch) };
     } break;
+		*/
   }
 
   /* Serialize instruction as string. */
@@ -255,9 +355,27 @@ char *pushInstructions(ProgramState &state,
   return s;
 }
 
+/* Generates a 30-entry program where each entry is a 37-vector
+ * with 16-bit float entries.
+ */
 char *makeProgram(uint32_t numInputs,
                   Rand &rnd)
 {
+	float *progMem = (float *) calloc(1, PROGRAM_SIZE_BYTES);
+	ProgramState programState = 
+	{
+		.programMemory = (char *) progMem,
+		.numInputs = numInputs,
+		.usedRegisters = 0
+	};
+	uint32_t instr_left = 10 + rnd.next() % 20;
+	while (instr_left--)
+	{
+		programState.programMemory = pushInstructions(programState, rnd);
+	}
+	return (char *) progMem;
+
+	/*
   char *programMemory = (char *)malloc(sizeof(char) * kMaxProgramSize);
   ProgramState programState = {
     .programMemory = programMemory,
@@ -272,6 +390,7 @@ char *makeProgram(uint32_t numInputs,
   }
 
   return programMemory;
+	*/
 }
 
 struct Program {
@@ -456,7 +575,10 @@ int *executeProgram(const Program &program)
       case Operation::Mov: {
         *leftContainer = *rightContainer;
       } break;
+		
+			default: {} break;
 
+			/*
       case Operation::BranchLess: {
         if (*leftContainer < 0) {
           int numLines = *rightContainer;
@@ -489,6 +611,7 @@ int *executeProgram(const Program &program)
           }
         }
       } break;
+			*/
     }
   }
 
