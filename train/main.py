ddb664490a037022804d663500ed44924679024b
Luc Rosenzweig <lucrosenzweig@Lucs-MacBook-Pro-3.local>
2023-11-22 23:17:00 -0500

    Added capability to see if the generated program is similar to the original

--- train/main.py
+++ train/main.py
@@ -80,15 +80,49 @@ def train_main(lr, rho):
 
 
 
-def test_main(model_path, example_idx):
+def translate_probabilities(out1, out2, out3):
     operations = [ 'add', 'sub', 'mul', 'div', 'mov' ]
     inputs = [ 'x0', 'x1', 'x2' ]
     registers = [ 'r0', 'r1', 'r2' ]
 
+    output = ''
+
+    for i in range(data.INSTRUCTIONS_PER_PROGRAM):
+        instruction_probs = out1[i]
+        lvalue_probs = out2[i]
+        rvalue_probs = out3[i]
+
+        op_idx = int(torch.argmax(instruction_probs))
+        lvalue_idx = int(torch.argmax(lvalue_probs))
+        rvalue_idx = int(torch.argmax(rvalue_probs))
+
+        instruction_str = operations[op_idx]
+        lvalue_str = None
+        rvalue_str = None
+
+        if lvalue_idx < 3:
+            lvalue_str = inputs[lvalue_idx]
+        else:
+            lvalue_str = registers[lvalue_idx - 3]
+
+        if rvalue_idx < 3:
+            rvalue_str = inputs[rvalue_idx];
+        elif rvalue_idx < 6:
+            rvalue_str = registers[rvalue_idx - 3]
+        else:
+            rvalue_str = str(rvalue_idx - 6)
+
+        output += instruction_str + ' ' + lvalue_str + ' ' + rvalue_str + '\n'
+
+    return output
+
+
+
+def test_main(model_path, example_idx):
     dataset = data.ProgDataset('../data/dataset/validation', NUM_VAL_EXAMPLES)
 
     io_pairs = dataset.load_io_pairs(example_idx)
-    print(io_pairs.shape)
+    label1, label2, label3 = dataset.load_label(example_idx)
 
     # Test output of the model
     model = torch.load(model_path)
@@ -97,33 +131,11 @@ def test_main(model_path, example_idx):
     model.eval()
     with torch.no_grad():
         out1, out2, out3 = model(io_pairs)
+        print("Predicted:")
+        print(translate_probabilities(out1, out2, out3))
 
-        for i in range(data.INSTRUCTIONS_PER_PROGRAM):
-            instruction_probs = out1[i]
-            lvalue_probs = out2[i]
-            rvalue_probs = out3[i]
-
-            op_idx = int(torch.argmax(instruction_probs))
-            lvalue_idx = int(torch.argmax(lvalue_probs))
-            rvalue_idx = int(torch.argmax(rvalue_probs))
-
-            instruction_str = operations[op_idx]
-            lvalue_str = None
-            rvalue_str = None
-
-            if lvalue_idx < 3:
-                lvalue_str = inputs[lvalue_idx]
-            else:
-                lvalue_str = registers[lvalue_idx - 3]
-
-            if rvalue_idx < 3:
-                rvalue_str = inputs[rvalue_idx];
-            elif rvalue_idx < 6:
-                rvalue_str = registers[rvalue_idx - 3]
-            else:
-                rvalue_str = str(rvalue_idx - 6)
-
-            print(instruction_str, lvalue_str, rvalue_str)
+        print("Original:")
+        print(translate_probabilities(label1, label2, label3))
 
 
 if __name__ == "__main__":
